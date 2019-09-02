d80e0bebe81f963f4a653253a29252e4d48a1220
zzyzjz <654277911@qq.com>
2019-09-02 17:07:07 +0800

    ...

---
+++ app/js/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :class="$style.home">
+        <h1>Home2</h1>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" module>
+  @import '../../css/reset.scss';
+  .home{
+    color: red;
+    font-size: 80px;
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,109 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+module.exports = env => {
+  if (!env) {
+    env = {}
+  }
+  let plugins = [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './app/views/index.html'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin()
+  ];
+  if (env.production) {
+    plugins.push(
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"'
+        }
+      }),
+      new ExtractTextPlugin("style.css", {
+        ignoreOrder: true
+      })
+    )
+  }
+  return {
+    entry: ['./app/js/viewport.js', './app/js/main.js'],
+    devServer: {
+      contentBase: './dist',
+      hot: true,
+      compress: true,
+      port: 9000,
+      clientLogLevel: "none",
+      quiet: true
+    },
+    module: {
+      rules: [{
+        test: /\.html$/,
+        use: ['cache-loader', 'html-loader']
+      }, {
+        test: /\.vue$/,
+        use: [
+          'cache-loader',
+          'vue-loader'
+        ]
+      }, {
+        test: /\.scss$/,
+        oneOf: [{
+          resourceQuery: /module/,
+          use: [
+            'vue-style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                localIdentName: '[local]_[hash:base64:5]'
+              }
+            }, {
+              loader: 'px2rem-loader',
+              options: {
+                remUnit: 40,
+                remPrecision: 8
+              }
+            },
+            'sass-loader'
+          ]
+        }, {
+          use: [
+            'vue-style-loader',
+            'css-loader', {
+              loader: 'px2rem-loader',
+              options: {
+                remUnit: 40,
+                remPrecision: 8
+              }
+            },
+            'sass-loader'
+          ]
+        }],
+      }, {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      }]
+    },
+    resolve: {
+      extensions: [
+        '.js', '.vue', '.json'
+      ],
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+      }
+    },
+    mode: 'development',
+    plugins,
+    output: {
+      filename: '[name].min.js',
+      path: path.resolve(__dirname, 'dist')
+    }
+  }
+};
